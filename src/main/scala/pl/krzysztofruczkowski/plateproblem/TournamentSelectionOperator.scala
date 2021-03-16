317f100465a7f3498046b373202919b7b2789509
tojatos <tojatos@gmail.com>
2021-03-16 19:42:00 +0100

    small refactor

--- src/main/scala/pl/krzysztofruczkowski/plateproblem/TournamentSelectionOperator.scala
+++ src/main/scala/pl/krzysztofruczkowski/plateproblem/TournamentSelectionOperator.scala
@@ -3,7 +3,6 @@ package pl.krzysztofruczkowski.plateproblem
 import scala.util.Random
 
 class TournamentSelectionOperator(random: Random) extends PlateSolutionSelectionOperator {
-  def select(population: List[ConcretePlateSolution]): ConcretePlateSolution = {
+  def select(population: List[ConcretePlateSolution]): ConcretePlateSolution =
     random.shuffle(population).take(Const.TOURNAMENT_SELECTION_N).maxBy(_.fitness)
-  }
 }
