5b35ca76e10a85b77924fad8d547549f76c21ca9
tojatos <tojatos@gmail.com>
2021-03-21 17:05:17 +0100

    iterations researched

--- src/main/scala/pl/krzysztofruczkowski/Main.scala
+++ src/main/scala/pl/krzysztofruczkowski/Main.scala
@@ -17,20 +17,20 @@ object Main extends App {
   implicit val random: Random = new Random(randomSeed)
   val t1 = System.nanoTime()
 
-  val researchIters = 1000
-  val researchTimes = 5
-
   val seriousProblemsWithNames = problems.zip(filenames.map(_.split('.')(0))).drop(1)
   val testPopulations = List(10, 100, 500, 1000, 2000, 5000, 10000)
   val testPopulationParameters = testPopulations.map(x => GeneticPlateProblemParameters(x))
+
+  val testTournamentSizes = List(1, 2, 3, 5, 10, 20, 100)
+  val testTournamentParameters = testTournamentSizes.map(x => GeneticPlateProblemParameters(operator = new TournamentSelectionOperator(x)))
 
   def get_filename(problemString: String, p: GeneticPlateProblemParameters): String =
     "research_results\\" + List(problemString, p.populationSize, p.operator.name, p.crossProbabilitiy, p.mutationProbabilitiy).mkString("_") + ".txt"
 
   def researchParameters(problemWithName: (PlateProblem,String), p: GeneticPlateProblemParameters): Unit = {
-    val bestFitnessList = (1 to researchTimes).par.map { _ =>
+    val bestFitnessList = (1 to Const.RESEARCH_TIMES).par.map { _ =>
       lazy val po = new GeneticPlateProblemOptimizer(problemWithName._1, p)
-      for (_ <- 1 to researchIters) {
+      for (_ <- 1 to Const.RESEARCH_ITERATIONS) {
         po.iterate()
       }
       po.best.fitness
@@ -41,29 +41,41 @@ object Main extends App {
     println(filename, resultString)
   }
 
-
-//  researchParameters(seriousProblemsWithNames.head, testPopulationParameters(1))
-
-  // no need to run in parallel as it takes 100% of CPU anyways
-  seriousProblemsWithNames.foreach { problemWithName =>
-    testPopulationParameters.foreach { p =>
-      println(s"Beginning testing ${get_filename(problemWithName._2, p)}")
-      researchParameters(problemWithName, p)
+  def researchGenerations() {
+    seriousProblemsWithNames.foreach { problemWithName =>
+//       val muchData = (1 to 10).map({ x =>
+          val params = GeneticPlateProblemParameters()
+          val po = new GeneticPlateProblemOptimizer(problemWithName._1, params)
+          val data = (1 to 2000).map { _ =>
+            po.iterate()
+            val best = po.best.fitness
+            val worst = po.population.minBy(_.fitness).fitness
+            val avg = po.population.map(_.fitness).sum / po.population.length
+            val std = Util.stdDev(po.population.map(_.fitness))
+            (best, worst, avg, std)
+          }
+//        data
+//      })
+//      (problemWithName._2, data)
+      val filename = s"research_results\\${problemWithName._2}.txt"
+      val stringData = data.mkString("\n")
+      File(filename).writeAll(stringData)
+      println(filename, stringData)
+//      muchData.flatMap(_.zipWithIndex).groupBy { case (num, idx) => idx }
+//        .map { case (k, v) => v.map(_._1).sum }.toList
     }
   }
-//  for (i <- 1 to 10) {
-//      val params = GeneticPlateProblemParameters()
-//      println(s"Generating random population (${Const.GENERIC_OPTIMIZER_POPULATION_SIZE})")
-//      val po = new GeneticPlateProblemOptimizer(problem, params)
-//      var duration = (System.nanoTime() - t1) / 1e9d
-//      println(s"Generated in $duration")
-//      for (j <- 1 to 200) {
-//        po.iterate()
-//      }
-//    println(s"Iteration $i")
-//    println(s"Best: ${po.best.fitness}")
-//    println(s"Avg: ${po.population.map(_.fitness).sum / po.population.length}")
-//    println(s"Worst: ${po.population.minBy(_.fitness).fitness}")
-//    println(s"Std dev: ${Util.stdDev(po.population.map(_.fitness))}")
-//  }
+
+
+  def research(parameters: List[GeneticPlateProblemParameters]) {
+    // no need to run in parallel as it takes 100% of CPU anyways
+    seriousProblemsWithNames.foreach { problemWithName =>
+      parameters.foreach { p =>
+        println(s"Beginning testing ${get_filename(problemWithName._2, p)}")
+        researchParameters(problemWithName, p)
+      }
+    }
+  }
+//  researchGenerations()
+  research(testTournamentParameters)
 }
